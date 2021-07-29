24945b953b5f8c2b7fad01d5eca3fcda5b5361ef
Shreyansh Kourav <74540509+specsx-codebot@users.noreply.github.com>
2021-07-29 13:37:17 +0530

    Added Bcrypt hash encryption

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express")
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const encrypt = require('mongoose-encryption')
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 // Configs
 const app = express()
@@ -19,10 +20,6 @@ const userSchema = new mongoose.Schema({
     username: String,
     password: String
 })
-
-//  Mongoose Encryption
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] })
-
 // Model
 const User = new mongoose.model("User", userSchema)
 
@@ -41,45 +38,44 @@ app.get("/login", function(req, res) {
 })
 
 app.post("/register", function(req, res) {
-    const userName = req.body.username
-    const password = req.body.password
 
-    const user = new User({
-        username: userName,
-        password: password
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+       const user = new User({
+            username: req.body.username,
+            password: hash
+        })
 
-    user.save(function(err) {
-        if (!err) {
-            res.render("secrets")
-        } else {
-            console.log(err)
-        }
-    })
+        user.save(function(err) {
+            if (!err) {
+                res.render("secrets")
+            } else {
+                console.log(err)
+            }
+        }) 
+    });
 })
 
 app.post("/login", function(req, res) {
-    const userName = req.body.username
-    const password = req.body.password
 
-    User.findOne({username: userName}, function(err, result) {
+    User.findOne({username: req.body.username}, function(err, foundUser) {
         if (!err) {
 
-            if (result) {
+            if (foundUser) {
 
-                if (result.password === password) {
-                    res.render("secrets")
-                } else {
-                    res.send("Please double check your password")
-                }
-                
+                bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+                    if (result === true) {   
+                        res.render("secrets")
+                    } else {
+                        res.send("Please double check your password")
+                    }
+                });
             } else {
                 res.send("Account not found please register first!")
             }
         
         } else {
             console.log(err)
-            res.send("Please double check your Username")
+            res.send("Please double check your entries or try again later")
         }
     })
 })
